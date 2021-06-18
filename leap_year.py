df1665f841a7c15d3784d8dc0b29cbfd7b51355d
Amla63 <86015356+Amla63@users.noreply.github.com>
2021-06-18 23:49:43 +0530

    Delete leap_year.py

--- leap_year.py
+++
@@ -1,8 +0,0 @@
-# year=int(input('enter the year'))
-# if year%4==0:
-# 	if year%400==0:
-# 		print('it is sentuary leap year')
-# 	else:
-# 		print('not sentuary leap year')
-# else:
-# 	print('it is leap year')
