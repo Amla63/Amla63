77ed6d7492015e1038f96411377a42f0fc8567a0
Amla63 <86015356+Amla63@users.noreply.github.com>
2021-06-18 23:44:51 +0530

    Delete age_oldest_youngest.py

--- age_oldest_youngest.py
+++
@@ -1,7 +0,0 @@
-# age=int(input('enter the number'))
-# age1=int(input('enter the number'))
-# age2=int(input('enter the number'))
-# if age>=age1 and age>=age2:
-# 	print('age is youngest')
-# else:
-# 	print('age is oldest')
