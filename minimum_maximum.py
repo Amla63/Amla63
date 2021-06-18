587a2f422c9f397c36e7ae618e759bb419c60368
Amla63 <86015356+Amla63@users.noreply.github.com>
2021-06-18 23:51:28 +0530

    Delete minimum_maximum.py

--- minimum_maximum.py
+++
@@ -1,6 +0,0 @@
-# number=int(input('enter the number'))
-# number1=int(input('enter the number'))
-# if number>number1:
-# 	print(number,'number maximum hai')
-# else:
-# 	print(number,'number minimum hai')
