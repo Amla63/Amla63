f369c09c5694474b618d0ed4a61965bfa2620dec
Amla63 <86015356+Amla63@users.noreply.github.com>
2021-06-18 23:49:21 +0530

    Delete isosles_tringle.py

--- isosles_tringle.py
+++
@@ -1,9 +0,0 @@
-# user=int(input('enter the number'))
-# user1=int(input('enter the number'))
-# user2=int(input('enter the number'))
-# if user==user1 and user1==user2 :
-# 	print(' triangle hai')
-# elif user==user1 and user1!=user2:
-# 	print("isosceles hai")
-# else:
-# 	print("not equal")
